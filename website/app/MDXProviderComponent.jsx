e452a138b1c3525f47b900d1730b9132a36f4dc1
Xinwei Xiong <3293172751NSS@gmail.com>
2025-04-24 23:20:54 +0800

    feat: remove unused content and add service templates for helm charts

--- website/app/MDXProviderComponent.jsx
+++ website/app/MDXProviderComponent.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+"use client"
 
-// 创建静态的MDX组件配置
-const mdxComponents = {
-  // 为不同元素定义基础样式
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+// 基础MDX组件
+const components = {
   h1: props => <h1 className="text-4xl font-bold mt-8 mb-4" {...props} />,
   h2: props => <h2 className="text-3xl font-bold mt-6 mb-3" {...props} />,
   h3: props => <h3 className="text-2xl font-bold mt-5 mb-2" {...props} />,
+  p: props => <p className="my-4" {...props} />,
   ul: props => <ul className="list-disc pl-5 my-4" {...props} />,
   ol: props => <ol className="list-decimal pl-5 my-4" {...props} />,
   li: props => <li className="my-1" {...props} />,
   a: props => <a className="text-blue-600 hover:underline" {...props} />,
-  p: props => <p className="my-4" {...props} />
+  blockquote: props => <blockquote className="border-l-4 border-gray-300 pl-4 italic my-4" {...props} />
 }
 
-// Next.js需要这个导出格式
-export function useMDXComponents(components) {
-  return { ...mdxComponents, ...components }
+// MDX提供器组件
+export default function MDXProviderComponent({ children }) {
+  return (
+    <MDXProvider components={components}>
+      {children}
+    </MDXProvider>
+  )
 } 
