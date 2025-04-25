476122c1bb0338cde95df2fb112e8717654dfb17
Xinwei Xiong <3293172751NSS@gmail.com>
2025-04-25 12:36:47 +0800

    feat: remove deprecated files and clean up project structure

--- website/app/blog/posts/layout.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function BlogPostsLayout({ children }) {
-  return (
-    <div className="blog-posts-layout">
-      {children}
-    </div>
-  )
-} 
--- website/app/components/ClientMDX.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import React from 'react'
-
-// Client MDX wrapper component
-// Used to wrap content that needs to use MDX components, ensuring they are rendered on the client side
-export default function ClientMDX({ children }) {
-  return <>{children}</>
-} 
