7a9260c94981da07b236f9a79e8651d1875f7da3
Xinwei Xiong <3293172751NSS@gmail.com>
2025-05-01 12:17:38 +0800

    feat: implement email utility functions for sending and managing emails

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,3 +1,21 @@
 from .posthog_tracker import PostHogTracker
+from .email import (
+    generate_password_reset_token,
+    generate_reset_password_email,
+    send_email,
+    verify_password_reset_token,
+    EmailData,
+    generate_test_email,
+    generate_new_account_email,
+)
 
-__all__ = ["PostHogTracker"] 
+__all__ = [
+    "PostHogTracker",
+    "generate_password_reset_token",
+    "generate_reset_password_email",
+    "send_email",
+    "verify_password_reset_token",
+    "EmailData",
+    "generate_test_email",
+    "generate_new_account_email",
+] 
